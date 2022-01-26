942d2337fb72d46ed889fce59926d4c8abb662b9
ANS <36783010+answebdev@users.noreply.github.com>
2022-01-25 20:59:36 -0800

    add routing and dummy views

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import spinner from '../img/spinner.gif';
-import classes from '../styles/Spinner.module.css';
 
 const Spinner = () => (
-  <img className={classes.SpinnerGif} src={spinner} alt='Loading...' />
+  <img
+    src={spinner}
+    alt='Loading...'
+    style={{ width: '35px', height: 'auto', margin: 'auto', display: 'block' }}
+  />
 );
 
 export default Spinner;
+
+// import React from 'react';
+// import spinner from '../img/spinner.gif';
+// import classes from '../styles/Spinner.module.css';
+
+// const Spinner = () => (
+//   <img className={classes.SpinnerGif} src={spinner} alt='Loading...' />
+// );
+
+// export default Spinner;
