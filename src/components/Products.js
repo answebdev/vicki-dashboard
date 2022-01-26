7b99e3b0271ae940d42e1c9c48614ea53eee544d
ANS <36783010+answebdev@users.noreply.github.com>
2022-01-25 20:36:51 -0800

    add routing and dummy views

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../src/img/viatouch_logo.png';
 import dashboard from '../img/dashboard.webp';
